eb95f4fe66866780c07da401ae0c710767ea3bc0
Anton Smolka <antonsmolka87@gmail.com>
2018-10-08 15:14:35 -0400

    Fixed $ Bug

--- lyricscraper.py
+++ lyricscraper.py
@@ -4,14 +4,16 @@ import requests
 def the_artist():
     artist = input("Enter an artists name: ")
     new_artist = artist.replace("&", "and")
-    new_artist =  "".join(c for c in new_artist if c not in "~`!@#$%^*()_+={}[]'|\:;?/.,<>`")
+    new_artist = new_artist.replace("$", "-")
+    new_artist =  "".join(c for c in new_artist if c not in "~`!@#%^*()_+={}[]'|\:;?/.,<>`")
     new_artist = new_artist.replace(" ", "-")
     return new_artist
 
 def the_song():
     song = input("Enter a song by the artist: ")
     new_song = song.replace("&", "and")
-    new_song =  "".join(c for c in new_song if c not in "~`!@#$%^*()_+={}[]'|\:;?/.,<>`")
+    new_song = new_song.replace("$", "-")
+    new_song =  "".join(c for c in new_song if c not in "~`!@#%^*()_+={}[]'|\:;?/.,<>`")
     new_song = new_song.replace(" ", "-")
     return new_song
 
@@ -24,7 +26,8 @@ def main():
 
     try:
         page = requests.get(url)
-        html = BeautifulSoup(page.text, "html.parser") # Extract the page's HTML as a string
+        #Extract the page's HTML as a string
+        html = BeautifulSoup(page.text, "html.parser")
         #Scrape the song lyrics from the HTML
         lyrics = html.find("div", class_="lyrics").get_text()
         print(lyrics)
